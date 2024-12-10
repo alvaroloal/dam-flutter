5baa27a6a74c7d5bf1e346c333e86060b77f4cda
alvaroloal <alorentealman@gmail.com>
2024-12-10 16:26:39 +0100

    ejercicios

--- Flutter/lista_horiz/android/app/src/main/kotlin/com/example/lista_horiz/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lista_horiz
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Flutter/lista_horizontal/android/app/src/main/kotlin/com/example/lista_horizontal/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lista_horizontal
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
