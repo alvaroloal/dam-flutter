ea08f4401b60eeb37856f558c5637b4722ca6dd5
alvaroloal <alorentealman@gmail.com>
2024-12-17 21:43:33 +0100

    estilos inicio y lista peliculas

--- Api_TMDB/tmdb/lib/screens/home_screen.dart
+++ Api_TMDB/tmdb/lib/screens/home_screen.dart
@@ -10,7 +10,6 @@ class _HomeScreenState extends State<HomeScreen> {
   final TMDBService _service = TMDBService();
   List<dynamic> _popularActors = [];
   List<dynamic> _popularMovies = [];
-  int _currentIndex = 0;
 
   @override
   void initState() {
@@ -37,19 +36,33 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text(
           'Inicio',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+            color: Colors.white,
+            letterSpacing: 1.5,
+          ),
         ),
         backgroundColor: Colors.deepPurpleAccent,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _buildSectionTitle('Películas Populares'),
-            _buildHorizontalList(_popularMovies, 'title', true),
-            _buildSectionTitle('Actores Populares'),
-            _buildHorizontalList(_popularActors, 'name', false),
-          ],
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.deepPurpleAccent, Color.fromARGB(255, 59, 19, 66)],
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildSectionTitle('Películas Populares'),
+              _buildHorizontalList(_popularMovies, 'title', true),
+              _buildSectionTitle('Actores Populares'),
+              _buildHorizontalList(_popularActors, 'name', false),
+            ],
+          ),
         ),
       ),
     );
@@ -61,57 +74,71 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Text(
         title,
         style: const TextStyle(
-            fontSize: 22, fontWeight: FontWeight.w600, color: Colors.black87),
+          fontSize: 22,
+          fontWeight: FontWeight.w600,
+          color: Colors.grey,
+        ),
       ),
     );
   }
 
   Widget _buildHorizontalList(List<dynamic> items, String key, bool isMovie) {
-    return Container(
-      height: 200,
+    return SizedBox(
+      height:
+          240, // Aumentar la altura de la lista horizontal para hacer las tarjetas más grandes
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: items.length,
         itemBuilder: (context, index) {
-          return Card(
-            margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-            shape: RoundedRectangleBorder(
+          return Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            decoration: BoxDecoration(
+              color: Colors.white,
               borderRadius: BorderRadius.circular(12),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black26,
+                  offset: Offset(0, 4),
+                  blurRadius: 6,
+                ),
+              ],
             ),
-            elevation: 4,
-            child: Container(
-              width: 130,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  ClipRRect(
-                    borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(12),
-                        topRight: Radius.circular(12)),
-                    child: Image.network(
-                      'https://image.tmdb.org/t/p/w500${items[index]['poster_path'] ?? items[index]['profile_path']}',
-                      height: 100,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                      errorBuilder: (context, error, stackTrace) => Container(
-                        color: Colors.grey,
-                        height: 130,
-                        child: const Icon(Icons.image_not_supported),
-                      ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    topRight: Radius.circular(12),
+                  ),
+                  child: Image.network(
+                    'https://image.tmdb.org/t/p/w500${items[index]['poster_path'] ?? items[index]['profile_path']}',
+                    height: 150,
+                    width: 160,
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) => Container(
+                      color: Colors.grey,
+                      height: 150,
+                      width: 160,
+                      child: const Icon(Icons.image_not_supported, size: 40),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      items[index][key] ?? 'Desconocido',
-                      textAlign: TextAlign.center,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(fontSize: 14),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    items[index][key] ?? 'Desconocido',
+                    textAlign: TextAlign.center,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black87,
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           );
         },
